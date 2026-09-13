ea386c665048b24ab8a73f7a279059d8e529193d
kirsikkab <kirsikka.bister@gmail.com>
2026-09-13 09:21:08 +0300

    Kirjautumisen ja rekisteröitymisen backend

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -5,8 +5,10 @@ interface NavbarProps {
   title: string; // Sivun otsikko, esim. "Uravalmennus"
   onOpenLogin: () => void;
   onOpenRegister: () => void;
+  loggedInUser: string | null;
+  onLogout: () => void;
 }
-export function Navbar({ title, onOpenLogin, onOpenRegister }: NavbarProps) {
+export function Navbar({ title, onOpenLogin, onOpenRegister, loggedInUser, onLogout, }: NavbarProps) {
   return (
     <header className="px-5 pt-5 pb-3">
       <div className="flex items-start justify-between">
@@ -16,23 +18,37 @@ export function Navbar({ title, onOpenLogin, onOpenRegister }: NavbarProps) {
 
         <div className="flex flex-col items-end gap-1">
 
-          {/* Kirjautumis- ja rekisteröitymisnapit*/}
-          <div className="flex gap-3 text-sm">
+          {/* kirjautunut käyttäjä / Kirjautumis- ja rekisteröitymisnapit*/}
+          {loggedInUser ? (
+            <div className="flex gap-3 text-sm">
+              <span className="text-foreground font-medium">
+                Kirjautuneena {loggedInUser}
+              </span>
 
-            <button
-              onClick={onOpenLogin}
-              className="text-foreground font-medium hover:text-primary transition-colors"
-            >
-              Kirjaudu
-            </button>
+              <button
+                onClick={onLogout}
+                className="text-foreground font-medium hover:text-primary transition-colors"
+              >
+                Kirjaudu ulos
+              </button>
+            </div>
+          ) : (
+            <div className="flex gap-3 text-sm">
+              <button
+                onClick={onOpenLogin}
+                className="text-foreground font-medium hover:text-primary transition-colors"
+              >
+                Kirjaudu
+              </button>
 
-            <button
-              onClick={onOpenRegister}
-              className="text-foreground font-medium hover:text-primary transition-colors"
-            >
-              Rekisteröidy
-            </button>
-          </div>
+              <button
+                onClick={onOpenRegister}
+                className="text-foreground font-medium hover:text-primary transition-colors"
+              >
+                Rekisteröidy
+              </button>
+            </div>
+          )}
 
           <div className="bg-primary text-primary-foreground text-xs font-bold px-3 py-1.5 rounded-lg tracking-wide mt-1">
             SLICE.FI
